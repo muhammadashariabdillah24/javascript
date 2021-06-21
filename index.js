64f6d228f7d4a8f6cccfdea42cfa63c56909af46
Abdillah7997 <tutordunet09@gmail.com>
2021-06-22 02:42:32 +0700

    Belajar Array

--- index.js
+++ index.js
@@ -19,21 +19,59 @@
 //     console.log('Benar 4')
 // }
 
-let angka = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let angka = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let angka_hasil = angka[1];
+//  let angka_hasil = angka[5]
 
-if (angka_hasil == 0) {
-    for (let i = 0; i >= 0;) {
-        console.log('Serangan Tipe 0 Sebanyak', i)
-        i+=1
-    }
-} else if(angka_hasil == 1) {
-    let i = 1;
-    while (i) {
-        console.log('Serangan Tipe 1 Sebanyak', i)
-        i+=2
-    }
-} else {
-    console.log('Maaf tidak ada angka yang sama , silahkan coba lagi')
+//  if (angka_hasil == 1 || 2 || 3 || 4 || 5) {
+//      for (let i = 0; i >= 0;) {
+//          console.log('Serangan Tipe 0 Sebanyak', i)
+//          i+=1
+
+//          if(i == 100){
+//              console.log('Lanjuttt')
+//              continue;
+//          }else if(i == 110){
+//              console.log('Stopp!!!')
+//              break;
+//          }
+//      }
+
+//  } else if(angka_hasil == 6 || 7 || 8 || 9 || 10) {
+//      let i = 1;
+//      while (i) {
+//          console.log('Serangan Tipe 1 Sebanyak', i)
+//         i+=2
+//      }
+//  } else {
+//      console.log('Maaf tidak ada angka yang sama , silahkan coba lagi')
+//  }
+
+// for (let i = 0; i >= 0;) {
+//     i++;
+
+//     if(i%2 == 0){
+//         console.log(i, ' Ini Bilangan Genap')
+//     }else{
+//         console.log(i, ' Ini Bilangan Ganjil');
+//     }
+// }
+
+function dataArray() {
+
+    let dataArr = [['Melon', 'Semangka', 'Jeruk'],
+                    ['Tomat', 'Bayam', 'Cabe'],
+                     ['Sepeda', 'Mobil', 'Motor']];
+
+    console.log(dataArr[0]);
+    console.log(dataArr[0][0] , ' Adalah buah terenak');
+    console.log(dataArr[1]);
+    console.log(dataArr[1][1] , ' Adalah sayuran terenak');
+    console.log(dataArr[2]);
+    console.log(dataArr[2][2] , ' Adalah kendaraan terbaik');
+    
 }
+
+dataArray();
+
+
